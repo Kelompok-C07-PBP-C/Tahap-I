c18037dd6866ee86c52b365636a6a6af75b75142
rinduaurellia <rindu.aurellia@ui.ac.id>
2025-10-24 18:07:16 +0700

    user interaction done : like and wishlist

--- like/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'like'
-
-urlpatterns = [
-    path('add/', views.add_like, name='add_like'),
-    path('delete/', views.delete_like_from_catalog, name='delete_like_from_catalog'),
-]
