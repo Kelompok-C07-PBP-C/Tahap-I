239b56edafbed29283129d8af6028149a614ea2f
rinduaurellia <rindu.aurellia@ui.ac.id>
2025-10-24 19:00:42 +0700

    fix forms.py

--- main/urls.py
+++ main/urls.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from main.views import *
-from django.urls import path
-from .views import VenueListCreateView
 
 app_name = 'main'
 
 urlpatterns = [
     path('login/', show_login, name="login"),
     path('register/', show_register, name="register"),
-    path('', show_landing, name="landing"),
-    path('logout/', logged_out, name="logout"),
-    path('venues/', VenueListCreateView.as_view(), name='venue-list'),
+    path('', show_landing, name="landing")
 ]
-
-
-
