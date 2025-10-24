571ef56470638c5b5142a1c69bdb872554026eaa
R. Pandji M. Dimaz B H D S <raden.pandji@ui.ac.id>
2025-10-24 23:34:26 +0700

    Refactor

--- TK_PBP/urls.py
+++ TK_PBP/urls.py
@@ -1,25 +1,17 @@
-"""
-URL configuration for TK_PBP project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""Root URL configuration for TK PBP project."""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
+from authentication.views import HomeView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("main.urls")),
-    path('api/', include('main.urls')),
+    path("admin/", admin.site.urls),
+    path("", HomeView.as_view(), name="home"),
+    path("", include("authentication.urls")),
+    path("workspace/", include("manajemen_lapangan.urls")),
+    path("", include("katalog.urls")),
+    path("", include("rent.urls")),
+    path("", include("interaksi.urls")),
+    path("favicon.ico", RedirectView.as_view(url="/static/images/favicon.ico")),
 ]
-
