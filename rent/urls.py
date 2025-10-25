1b3f78eb35ebe1223845af40f649c68729274fe2
tirrexx-x64 <tirtarendysi77@gmail.com>
2025-10-25 14:03:19 +0700

    hampir final

--- rent/urls.py
+++ rent/urls.py
@@ -12,7 +12,7 @@ from .views import (
 urlpatterns = [
     path("bookings/", BookedPlacesView.as_view(), name="booked-places"),
     path("bookings/<int:pk>/cancel/", BookingCancelView.as_view(), name="booking-cancel"),
-    path("bookings/<int:pk>/payment/", BookingPaymentView.as_view(), name="booking-payment"),
+    path("bookings/<int:pk>/payment/", BookingPaymentView.as_view(), name="payment"),
     path("bookings/json/", BookedPlacesJSONView.as_view(), name="booked-places-json"),
     path("bookings/<int:pk>/payment/json/", BookingPaymentJSONView.as_view(), name="booking-payment-json"),
 ]
